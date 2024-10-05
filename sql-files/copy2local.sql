b066b251fb53c29e92e4ed48866ff3ec078a67ad
AnJung wasabi-neko <wasabi.neko.owo@gmail.com>
2024-10-06 00:38:46 +0800

    Fix: remove redundant code

--- sql-files/copy2local.sql
+++ sql-files/copy2local.sql
@@ -12,12 +12,3 @@ SELECT
     PULOCATIONID, DOLOCATIONID,
     EXTRA, TIP_AMOUNT, AIRPORT_FEE
 from super_extable;
-
-desc table final_table;
-select start_date, sum(passenger_count) from final_table
-group by start_date
-order by start_date;
-
-select year(start_date), sum(passenger_count) from final_table
-group by year(start_date) 
-order by year(start_date);
