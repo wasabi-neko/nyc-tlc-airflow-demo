ddf044ab0290562c683fca330b32f9db1a26b9bd
AnJung wasabi-neko <wasabi.neko.owo@gmail.com>
2024-10-08 17:17:20 +0800

    Add sql file

---
+++ sql-files/create_aws_intergration.sql
@@ -0,0 +1,10 @@
+-- Create integration with AWS
+CREATE or REPLACE STORAGE INTEGRATION s3_integration
+  TYPE = EXTERNAL_STAGE
+  STORAGE_PROVIDER = 'S3'
+  ENABLED = TRUE
+  STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::<role_iam>'
+  STORAGE_ALLOWED_LOCATIONS = ('*');
+  -- STORAGE_BLOCKED_LOCATIONS = (');
+
+DESC INTEGRATION s3_integration;
