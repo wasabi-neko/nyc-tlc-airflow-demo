f8829cd8b9444a8d0ca4206fdb83066a0091bd89
AnJung wasabi-neko <wasabi.neko.owo@gmail.com>
2024-10-09 12:56:29 +0800

    Update sql query query history version

--- sql-files/query_query_history.sql
+++ sql-files/query_query_history.sql
@@ -2,18 +2,35 @@ use warehouse compute_wh;
 
 desc table snowflake.account_usage.query_history;
 
+-- ----------------------------------------
+-- V2 query
+-- ----------------------------------------
+
+select 
+    query_id, 
+    query_text,
+    try_parse_json(query_tag)['dag_timestamp'],
+    try_parse_json(query_tag)['type'],
+    CAST(try_parse_json(query_tag)['dag_timestamp'] AS datetime)as dag_timestamp
+from snowflake.account_usage.query_history
+where
+    try_parse_json(query_tag)['project']::string = 'nyc-tlc-demo-compare-v2' 
+order by 
+    try_parse_json(query_tag)['dag_timestamp'] desc
+;
+
 select
     count(QUERY_ID),
-    try_parse_json(query_tag)['type']::string as mtype,
-    SUM(total_elapsed_time) as sum_time,
-    timediff(second, min(start_time), MAX(end_time)) as real_duration,
+    try_parse_json(query_tag)['type']::string as query_type,
+    SUM(total_elapsed_time) as sum_total_elaspe_time_ms,
+    timediff(second, min(start_time), MAX(end_time)) as real_duration_sec,
+    CAST(try_parse_json(query_tag)['dag_timestamp'] AS datetime)as dag_timestamp,
     MIN(start_time),
     MAX(end_time),
-    CAST(try_parse_json(query_tag)['dag_timestamp'] AS datetime)as dag_timestamp
 from
     snowflake.account_usage.query_history
 where 
-    try_parse_json(query_tag)['project']::string = 'nyc-tlc-demo-compare-v1' and
+    try_parse_json(query_tag)['project']::string = 'nyc-tlc-demo-compare-v2' and
     execution_status = 'SUCCESS'
 
 GROUP BY
@@ -30,7 +47,7 @@ select
     CAST(try_parse_json(query_tag)['dag_timestamp'] AS datetime)as dag_timestamp
 from snowflake.account_usage.query_history
 where
-    try_parse_json(query_tag)['project']::string = 'nyc-tlc-demo-compare-v1' 
+    try_parse_json(query_tag)['project']::string = 'nyc-tlc-demo-compare-v2' 
 order by 
     try_parse_json(query_tag)['dag_timestamp'] desc
 ;
@@ -45,7 +62,7 @@ select
     CAST(try_parse_json(query_tag)['dag_timestamp'] AS datetime)as dag_timestamp
 from snowflake.account_usage.query_history
 where 
-    try_parse_json(query_tag)['project']::string = 'nyc-tlc-demo-compare-v1' and
+    try_parse_json(query_tag)['project']::string = 'nyc-tlc-demo-compare-v2' and
     try_parse_json(query_tag)['type']::string = 'external_stage' and
     execution_status = 'SUCCESS'
 
@@ -55,6 +72,6 @@ ORDER BY real_duration desc;
 
 select query_id, query_text, warehouse_name, query_load_percent
 from snowflake.account_usage.query_history
-where try_parse_json(query_tag)['project']::string = 'nyc-tlc-demo-compare-v1'
+where try_parse_json(query_tag)['project']::string = 'nyc-tlc-demo-compare-v2'
 order by try_parse_json(query_tag)['dag_timestamp'] desc;
 
